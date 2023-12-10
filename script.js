96ecea08ae2dc1f4ff6a0b145489353f864d08d6
jeder-Bertoldo <115741406+jeder-Bertoldo@users.noreply.github.com>
2023-12-10 14:23:20 -0500

    responsividade 50%

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+const menu = document.querySelector(".menu");
+const Navmenu = document.querySelector(".nav-menu");
 
-  
+menu.addEventListener('click', () =>{
+    menu.classList.toggle('ativo');
+    Navmenu.classList.toggle('ativo');
+})
   
