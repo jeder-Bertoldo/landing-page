08dee45ad549578e0f9c37d4d4005a93b926c8c1
jeder-Bertoldo <115741406+jeder-Bertoldo@users.noreply.github.com>
2023-12-12 13:30:54 -0500

    dark mode e light mode

--- script.js
+++ script.js
@@ -5,4 +5,9 @@ menu.addEventListener('click', () =>{
     menu.classList.toggle('ativo');
     Navmenu.classList.toggle('ativo');
 })
+const chk = document.getElementById('chk')
+
+chk.addEventListener('change', () => {
+  document.body.classList.toggle('dark')
+})
   
